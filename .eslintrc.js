dc3e1fb8b3b977fc43e62c574220acf24c525fa3
SamuelFDEZS <thggsamuel@gmail.com>
2025-10-06 22:00:55 +0200

    First commit & Configure linter

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    env: {
+        browser: true,
+        node: true,
+        es2021: true
+    },
+    extends: ['standard'],
+    rules: {
+        indent: ['error', 4],
+        semi: ['error', 'always'],
+        'prefer-const': 'off'
+    }
+};
