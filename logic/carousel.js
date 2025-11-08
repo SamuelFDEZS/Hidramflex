aee98fdb165e5d0897fde6030663b1140b46f13d
SamuelFDEZS <thggsamuel@gmail.com>
2025-11-08 14:53:51 +0100

    Create logic for carousel

--- logic/carousel.js
+++ logic/carousel.js
@@ -1,15 +1,51 @@
 let index = 0;
-const carousel = document.querySelector('.carrusel');
-const slides = carousel.querySelectorAll('.carrusel__img');
+let isAnimating = false;
+let beforePreviousSlide = null;
+const slides = document.querySelectorAll('.carrusel__img');
+const buttonLeft = document.querySelector('.carrusel__left-button');
+const buttonRight = document.querySelector('.carrusel__right-button');
 
-setInterval(() => {
-    slides.forEach(slide => slide.classList.remove('active'));
-    index = (index + 1) % slides.length;
-    slides[index].classList.add('active');
-    const slideWidth = carousel.clientWidth;
+let previousSlide = slides[slides.length - 1];
+const changeSlide = (direction) => {
+    if (isAnimating) return;
+    isAnimating = true;
 
-    carousel.scrollTo({
-        left: slideWidth * index,
-        behavior: 'smooth'
-    });
-}, 4000);
+    if (previousSlide) previousSlide.classList.remove('last', 'before-last');
+    if (beforePreviousSlide) previousSlide.classList.remove('last', 'before-last');
+
+    const current = slides[index];
+    const nextIndex = (index + direction + slides.length) % slides.length;
+    const next = slides[nextIndex];
+
+    next.classList.remove('last');
+    next.classList.add('active');
+    current.classList.remove('active');
+
+    if (direction === 1) {
+        current.classList.add('last');
+    } else {
+        current.classList.add('before-last');
+    }
+
+    previousSlide = current;
+    beforePreviousSlide = slides[(index - 2 + slides.length) % slides.length];
+    console.log(beforePreviousSlide);
+    index = nextIndex;
+    setTimeout(() => {
+        previousSlide.classList.remove('before-last');
+        previousSlide.classList.remove('last');
+        beforePreviousSlide.classList.add('last');
+
+        setTimeout(() => (isAnimating = false), 1000);
+    }, 1000);
+};
+
+const autoplay = setInterval(() => changeSlide(+1), 4000);
+buttonLeft.addEventListener('click', () => changeSlide(-1));
+buttonRight.addEventListener('click', () => changeSlide(+1));
+
+buttonLeft.addEventListener('mouseenter', () => clearInterval(autoplay));
+buttonRight.addEventListener('mouseenter', () => clearInterval(autoplay));
+
+buttonRight.addEventListener('mouseenter', () => clearInterval(autoplay));
+buttonRight.addEventListener('mouseenter', () => clearInterval(autoplay));
