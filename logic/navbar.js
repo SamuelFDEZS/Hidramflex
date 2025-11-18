162ae9c5c0124aaa801fda306997e7fd5dbf0315
SamuelFDEZS <thggsamuel@gmail.com>
2025-11-18 14:30:51 +0100

    Create responsive for computer sizes

--- logic/navbar.js
+++ logic/navbar.js
@@ -4,12 +4,20 @@ if ('scrollRestoration' in history) {
 
 const navBar = document.querySelector('.header');
 let scrollTop = 0;
+let threshold = 0;
 
+const updateThreshold = () => {
+    const headerHeight = navBar.offsetHeight;
+    threshold = window.innerHeight * 0.05 + headerHeight;
+};
 window.addEventListener('load', () => {
+    updateThreshold();
     requestAnimationFrame(() => {
         window.scrollTo(0, 0);
     });
 });
+
+window.addEventListener('resize', updateThreshold);
 
 window.addEventListener('scroll', () => {
     let { pageYOffset } = window;
